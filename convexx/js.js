c3fa412cd0610b40887bc7b275f6bd37874b9564
Vladislav Molodykh <vladislavmolodykh@gmail.com>
2024-11-11 19:39:47 +0600

    first commit

---
+++ convexx/js.js
@@ -0,0 +1,12 @@
+let login = document.querySelector('.login')
+let register = document.querySelector('.register')
+
+let showLogin = ()=>{
+    login.classList.toggle('active')
+}
+
+let showRegister = ()=>{
+    register.classList.toggle('active')
+}
+
+
---
+++ js.js
@@ -0,0 +1,9 @@
+let log = document.querySelector('.header__log')
+let reg = document.querySelector('.header__reg')
+
+let showboxlog = ()=>{
+    log.classList.toggle('log')
+}
+let showboxreg = ()=>{
+    reg.classList.toggle('reg')
+}
